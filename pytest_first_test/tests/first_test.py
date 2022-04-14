85575f9868025b25c2870f8cc8dab39818de1b46
semenchanika <95244099+semenchanika@users.noreply.github.com>
2022-04-14 20:44:08 +0300

    Update first_test.py

--- pytest_first_test/tests/first_test.py
+++ pytest_first_test/tests/first_test.py
@@ -11,3 +11,14 @@ class TestCalc:
     def test_multiply_calculate_failed(self):
         assert self.calc.multiply(self, 2, 2) == 5
 
+    def test_multiply_calculate_failed(self):
+        assert self.calc.multiply(self, 2, 2) == 5
+
+    def test_division_calculate_correctly(self):
+        assert self.calc.division(self,6,3)==2
+
+    def test_subtraction_calculate_correctly(self):
+        assert self.calc.subtraction(self, 10, 3) == 7
+
+    def test_adding_calculate_correctly(self):
+        assert self.calc.adding(self, 10, 3) == 13
